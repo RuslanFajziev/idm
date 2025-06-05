ea94cfee27a03cfea37840996386a98bb4e6798e
FaizievRT <rus23f@mail.ru>
2025-06-05 23:17:19 +0300

    Update migration scripts

--- migrations/20250603224245_9_add_migration.sql
+++ migrations/20250603224245_9_add_migration.sql
@@ -4,18 +4,20 @@ CREATE TABLE IF NOT EXISTS "employee"
 (
     "id" bigint GENERATED ALWAYS AS IDENTITY,
     "name" text not null,
-    "create_at" timestamptz not null,
-    "updated_at" timestamptz,
-	primary key ("id")
+    "create_at" timestamptz DEFAULT now(),
+    "update_at" timestamptz DEFAULT now(),
+
+    primary key ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "role"
 (
     "id" bigint GENERATED ALWAYS AS IDENTITY,
     "name" text not null,
-    "create_at" timestamptz not null,
-    "updated_at" timestamptz,
-	primary key ("id")
+    "create_at" timestamptz DEFAULT now(),
+    "update_at" timestamptz DEFAULT now(),
+
+    CONSTRAINT "pk_role_name" PRIMARY KEY ("name")
 );
 -- +goose StatementEnd
 
