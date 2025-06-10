45e11baf30ab6eda1f971d57eb46323e46c10c71
FaizievRT <rus23f@mail.ru>
2025-06-11 01:06:26 +0300

    Add layer service and fix error

---
+++ inner/employee/dto.go
@@ -0,0 +1,52 @@
+package employee
+
+import (
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type Entity struct {
+	Id     int64     `db:"id"`
+	Name   string    `db:"name"`
+	Create time.Time `db:"create_at"`
+	Update time.Time `db:"update_at"`
+}
+
+type Response struct {
+	Id     int64     `json:"id"`
+	Name   string    `json:"name"`
+	Create time.Time `json:"create_at"`
+	Update time.Time `json:"update_at"`
+}
+
+type Request struct {
+	Name   string    `json:"name"`
+	Create time.Time `json:"create_at"`
+	Update time.Time `json:"update_at"`
+}
+
+func (e *Entity) toResponse() Response {
+	return Response{
+		Id:     e.Id,
+		Name:   e.Name,
+		Create: e.Create,
+		Update: e.Update,
+	}
+}
+
+func toResponses(entities []Entity) (responses []Response) {
+	for _, e := range entities {
+		responses = append(responses, e.toResponse())
+	}
+
+	return responses
+}
+
+func (r *Request) toEntity() *Entity {
+	return &Entity{
+		Name:   r.Name,
+		Create: r.Create,
+		Update: r.Update,
+	}
+}
