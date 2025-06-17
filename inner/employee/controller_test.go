2c4dc90eb361a0366ad3d35765c4ca249fec9a20
FaizievRT <rus23f@mail.ru>
2025-06-17 20:51:10 +0300

    TestCreateEmployee

---
+++ inner/employee/controller_test.go
@@ -0,0 +1,159 @@
+package employee
+
+import (
+	"encoding/json"
+	"fmt"
+	"idm/inner/common"
+	"idm/inner/web"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gofiber/fiber"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// Объявляем структуру мока сервиса employee.Service
+type MockService struct {
+	mock.Mock
+}
+
+// Реализуем функции мок-сервиса
+// Реализуем функции мок-сервиса
+func (srv *MockService) FindById(id int64) (Response, error) {
+	args := srv.Called(id)
+	return args.Get(0).(Response), args.Error(1)
+}
+
+func (srv *MockService) SaveTx(req Request) (id int64, err error) {
+	args := srv.Called(req)
+	return args.Get(0).(int64), args.Error(1)
+}
+
+func (srv *MockService) FindByIds(ids []int64) ([]Response, error) {
+	args := srv.Called(ids)
+	return args.Get(0).([]Response), args.Error(1)
+}
+
+func (srv *MockService) GetAll() ([]Response, error) {
+	args := srv.Called()
+	return args.Get(0).([]Response), args.Error(1)
+}
+
+func (srv *MockService) DeleteById(id int64) error {
+	args := srv.Called(id)
+	return args.Error(0)
+}
+
+func (srv *MockService) DeleteByIds(ids []int64) error {
+	args := srv.Called(ids)
+	return args.Error(0)
+}
+
+func TestCreateEmployee(t *testing.T) {
+	var a = assert.New(t)
+
+	// тестируем положительный сценарий: работника создали и получили его id
+	t.Run("should return created employee id", func(t *testing.T) {
+		// Готовим тестовое окружение
+		server := web.NewServer()
+		var svc = new(MockService)
+		var controller = NewController(server, svc)
+		controller.RegisterRoutes()
+		// Готовим тестовое окружение
+		var body = strings.NewReader("{\"name\": \"john doe\"}")
+		var req = httptest.NewRequest(fiber.MethodPost, "/api/v1/employees", body)
+		req.Header.Set("Content-Type", "application/json")
+
+		// Настраиваем поведение мока в тесте
+		svc.On("SaveTx", mock.AnythingOfType("Request")).Return(int64(123), nil)
+
+		// Отправляем тестовый запрос на веб сервер
+		resp, err := server.App.Test(req)
+
+		// Выполняем проверки полученных данных
+		a.Nil(err)
+		a.NotEmpty(resp)
+		a.Equal(http.StatusOK, resp.StatusCode)
+		bytesData, err := io.ReadAll(resp.Body)
+		a.Nil(err)
+		var responseBody common.ResponseBody[int64]
+		err = json.Unmarshal(bytesData, &responseBody)
+		a.Nil(err)
+		a.Equal(int64(123), responseBody.Data)
+		a.True(responseBody.Success)
+		a.Empty(responseBody.Message)
+	})
+
+	t.Run("should return error when saving employee", func(t *testing.T) {
+		// Готовим тестовое окружение
+		server := web.NewServer()
+		var svc = new(MockService)
+		var controller = NewController(server, svc)
+		controller.RegisterRoutes()
+		// Готовим тестовое окружение
+		var body = strings.NewReader("{\"name\": \"john doe\"}")
+		var req = httptest.NewRequest(fiber.MethodPost, "/api/v1/employees", body)
+		req.Header.Set("Content-Type", "application/json")
+
+		body = strings.NewReader("{\"name\": \"john doe\"}")
+		req = httptest.NewRequest(fiber.MethodPost, "/api/v1/employees", body)
+		req.Header.Set("Content-Type", "application/json")
+		// Настраиваем поведение мока в тесте
+		var errMess = fmt.Errorf("employee with name %s already exists", "john doe").Error()
+		svc.On("SaveTx", mock.AnythingOfType("Request")).Return(int64(0),
+			common.AlreadyExistsError{Message: errMess})
+
+		// Отправляем тестовый запрос на веб сервер
+		resp, err := server.App.Test(req)
+		a.Nil(err)
+		a.NotEmpty(resp)
+		a.Equal(http.StatusBadRequest, resp.StatusCode)
+		bytesData, err := io.ReadAll(resp.Body)
+		a.Nil(err)
+		var responseBody common.ResponseBody[string]
+		err = json.Unmarshal(bytesData, &responseBody)
+		a.Nil(err)
+		a.False(responseBody.Success)
+		a.NotEmpty(responseBody.Message)
+		a.Equal(errMess, responseBody.Message)
+	})
+
+	t.Run("should return error when saving employee v2", func(t *testing.T) {
+		// Готовим тестовое окружение
+		server := web.NewServer()
+		var svc = new(MockService)
+		var controller = NewController(server, svc)
+		controller.RegisterRoutes()
+		// Готовим тестовое окружение
+		var body = strings.NewReader("{\"name\": \"john doe\"}")
+		var req = httptest.NewRequest(fiber.MethodPost, "/api/v1/employees", body)
+		req.Header.Set("Content-Type", "application/json")
+
+		body = strings.NewReader("{\"name\": \"john doe\"}")
+		req = httptest.NewRequest(fiber.MethodPost, "/api/v1/employees", body)
+		req.Header.Set("Content-Type", "application/json")
+		// Настраиваем поведение мока в тесте
+		var errMess1 = fmt.Errorf("database error")
+		var errMess2 = fmt.Errorf("error finding employee by name: %s, %w", "john doe", errMess1).Error()
+		svc.On("SaveTx", mock.AnythingOfType("Request")).Return(int64(0),
+			common.DbOperationError{Message: errMess2})
+
+		// Отправляем тестовый запрос на веб сервер
+		resp, err := server.App.Test(req)
+		a.Nil(err)
+		a.NotEmpty(resp)
+		a.Equal(http.StatusInternalServerError, resp.StatusCode)
+		bytesData, err := io.ReadAll(resp.Body)
+		a.Nil(err)
+		var responseBody common.ResponseBody[string]
+		err = json.Unmarshal(bytesData, &responseBody)
+		a.Nil(err)
+		a.False(responseBody.Success)
+		a.NotEmpty(responseBody.Message)
+		a.Equal(errMess2, responseBody.Message)
+	})
+}
