80e9c4792a822e9c7ce98300e34a51c8ca27a00e
FaizievRT <rus23f@mail.ru>
2025-06-25 00:34:24 +0300

    Add Gracefull Shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"idm/inner/common"
 	"idm/inner/database"
@@ -9,6 +10,10 @@ import (
 	"idm/inner/role"
 	"idm/inner/validator"
 	"idm/inner/web"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -24,6 +29,7 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
 	// закрываем соединение с базой данных после выхода из функции main
 	defer func() {
 		if err := database.Close(); err != nil {
@@ -31,10 +37,43 @@ func main() {
 		}
 	}()
 	var server = build(database, cfg)
-	err = server.App.Listen(":8080")
-	if err != nil {
-		panic(fmt.Sprintf("http server error: %s", err))
-	}
+
+	// Запускаем сервер в отдельной горутине
+	go func() {
+		err := server.App.Listen(":8080")
+		if err != nil {
+			panic(fmt.Sprintf("http server error: %s", err))
+		}
+	}()
+
+	// Создаем группу для ожидания сигнала завершения работы сервера
+	var wg = &sync.WaitGroup{}
+	wg.Add(1)
+
+	// Запускаем gracefulShutdown в отдельной горутине
+	go gracefulShutdown(server, wg)
+
+	// Ожидаем сигнал от горутины gracefulShutdown, что сервер завершил работу
+	wg.Wait()
+	fmt.Println("Graceful shutdown complete.")
+}
+
+// Функция "элегантного" завершения работы сервера по сигналу от операционной системы
+func gracefulShutdown(server *web.Server, wg *sync.WaitGroup) {
+	// Уведомить основную горутину о завершении работы
+	defer wg.Done()
+	// Создаём контекст, который слушает сигналы прерывания от операционной системы
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
+	defer stop()
+	// Слушаем сигнал прерывания от операционной системы
+	<-ctx.Done()
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+	// Контекст используется для информирования веб-сервера о том,
+	// что у него есть 5 секунд на выполнение запроса, который он обрабатывает в данный момент
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	fmt.Println("Server exiting")
 }
 
 // buil функция, конструирующая наш веб-сервер
